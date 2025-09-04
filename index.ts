6cb5240a4af150fe050bfb1bb06b49152d98e6cf
mikkpav <mikk.pavelson@gmail.com>
2025-09-04 22:10:51 +0300

    Take out auth requirement.

--- index.ts
+++ index.ts
@@ -69,7 +69,7 @@ app.use('/movies', movieRoutes);
 // Get all favorite movies' details
 //
 
-app.use('/favorites', requireAuth, favoritesRoutes);
+app.use('/favorites', favoritesRoutes); // Add ths when auth is ready: requireAuth, 
 
 //
 // Generic error handling
